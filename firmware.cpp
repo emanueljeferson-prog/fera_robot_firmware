22f58b1b0b6f71727f87f0afb3abc1a485453065
Jeffr <emanueljeferson@gmail.com>
2026-06-01 19:02:10 -0300

    Updating main file

--- firmware.cpp
+++ firmware.cpp
@@ -4,21 +4,25 @@
 #include "application/motor.hpp"
 #include "service/rtos.hpp"
 #include "service/encoder.hpp"
+#include "service/motor_drive.hpp"
 
 int main() {
     auto broker = core::Middleware(); 
     auto rtos = service::Rtos(broker);
     auto encoder = service::Encoder(broker);
-    auto motor = app::Motor(0, broker);
+    auto motor_drive = service::MotorDrive(broker);
+    auto motor = app::Motor(1, broker);
 
     for(size_t i = 0; i < config::motorCount; ++i) {
         const auto& motorCfg = config::motorConfigs[i];
         encoder.registerEncoder(motorCfg.encoder.pin_a, motorCfg.encoder.pin_b);
+        motor_drive.registerMotor(motorCfg.driver.pin_a, motorCfg.driver.pin_b);
     }
 
     // services 
     rtos.init();
     encoder.init();
+    motor_drive.init();
     // applications
     motor.init();
     // middleware
