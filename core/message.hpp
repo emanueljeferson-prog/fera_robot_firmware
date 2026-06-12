b781404228ea3c370e2ffeeaf38adfe0d445a4af
Jeffr <emanueljeferson@gmail.com>
2026-06-12 07:33:23 -0300

    Adding messages and protocol type message

--- core/message.hpp
+++ core/message.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 #include <functional>
 
 namespace core {
@@ -12,12 +13,29 @@ using Callback = std::function<void(const Message&)>;
 using TaskFunction = void (*)(void*);
 
 enum Topics {
-    MOTOR_CONTROL,
+    MOTOR_COMMAND,
     READ_SPEED,
     READ_IMU,
     READ_GPS,
     REGISTER_TASK,
-    DEALY_TASK
+    DELAY_TASK,
+    UROS_SPEED,
+    UROS_CMD,
+    UROS_IMU,
+    UROS_GPS,
+    EXTERN_MESSAGE_SEND
+};
+
+enum ProtocolType {
+    UART,
+    SPI,
+    I2C,
+    USB,
+    CAN,
+    FlexRay,
+    LIN,
+    WIFI,
+    ETHERNET
 };
 
 struct TaskDescription {
@@ -27,7 +45,6 @@ struct TaskDescription {
     TaskFunction task;
     void* parameters;
 };
-
 
 struct Message {
 public:
@@ -62,15 +79,59 @@ public:
     double& speed;
 };
 
-struct ControlMotorMessage: public Message {
+struct MotorCommandMessage: public Message {
 public: 
-    ControlMotorMessage(const uint8_t id, const int16_t signal, const Topics tp)
+    MotorCommandMessage(const uint8_t id, const int16_t signal, const Topics tp)
     : id(id), signal(signal), Message(tp) {}
     const uint8_t id;
     const int16_t signal;
 };   
 
+struct MicroRosMessageSpeed: public Message {
+public:
+    MicroRosMessageSpeed(const double speed_1, const double speed_2, const Topics tp)
+    : speed_1(speed_1), speed_2(speed_2), Message(tp) {}
+    const double speed_1;
+    const double speed_2;
+};
+
+struct MicroRosMessageImu: public Message {
+public:
+    MicroRosMessageImu(std::vector<int16_t> accel, std::vector<int16_t> gyro, std::vector<int16_t> mag, const int16_t temp, const Topics tp)
+    : accel(std::move(accel)), gyro(std::move(gyro)), mag(std::move(mag)), temp(temp), Message(tp) {}
+    std::vector<int16_t> accel;
+    std::vector<int16_t> gyro;
+    std::vector<int16_t> mag;
+    const uint16_t temp; 
+};
+
+struct MicroRosMessageGps: public Message {
+public:
+    MicroRosMessageGps(const double latitude, const double longitude, const double altitude, const uint32_t seconds, const uint32_t nanoseconds, const Topics tp)
+    : latitude(latitude), longitude(longitude), altitude(altitude), seconds(seconds), nanoseconds(nanoseconds), Message(tp) {}
+    const double latitude;
+    const double longitude;
+    const double altitude;
+    const uint32_t seconds;
+    const uint32_t nanoseconds;
+};
+
+struct ExternMessageSend: public Message {
+public: 
+    ExternMessageSend(const std::vector<uint8_t> payload, const ProtocolType protocol, const uint8_t address, const Topics tp) 
+    : payload(std::move(payload)), protocol(protocol), address(address), Message(tp){}
+    const std::vector<uint8_t> payload;
+    const ProtocolType protocol;
+    const uint8_t address; 
+};
+
+struct ExternMessageReceive: public Message {
+public: 
+    ExternMessageReceive(std::vector<uint8_t>& payload, const ProtocolType protocol, const uint8_t address, const Topics tp) 
+    : payload(payload), protocol(protocol), address(address), Message(tp){}
+    std::vector<uint8_t>& payload;
+    const ProtocolType protocol;
+    const uint8_t address;
+};
+
 }
-
-
-
